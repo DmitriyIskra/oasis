27080e9d7f2c6ad53a473eea8f64c4c362e719e1
Dmitriy Iskra <dmitriyiskra@mail.ru>
2024-11-21 17:59:22 +0300

    created catalog nav for desctop

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import app from './js/app';
 
 import './css/reset.css';
 import './css/fonts.css';
-import './css/footer.css';
 import './css/header.css';
+import './css/component-nav-catalog.css';
+import './css/footer.css';
