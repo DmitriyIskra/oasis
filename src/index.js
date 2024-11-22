2d7b5d0193ea2795a63137477738a8e8f7407b8d
Dmitriy Iskra <dmitriyiskra@mail.ru>
2024-11-22 13:27:14 +0300

    creating header

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import app from './js/app';
 
 
 import './css/reset.css';
+import './css/layout.css';
 import './css/fonts.css';
 import './css/header.css';
+import './css/index.css';
 import './css/component-nav-catalog.css';
+import './css/component-nav-bar-mobile.css';
 import './css/footer.css';
