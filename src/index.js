f21221959589e7181181e651fff0559ed651b647
Dmitriy Iskra <dmitriyiskra@mail.ru>
2024-11-25 13:20:52 +0300

    creating mobile catalog

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './css/fonts.css';
 import './css/header.css';
 import './css/index.css';
 import './css/component-nav-catalog.css';
+import './css/component-burger.css';
 import './css/component-nav-bar-mobile.css';
 import './css/component-nav-mobile.css';
 import './css/footer.css';
