d96eaa76fc1d7ce4d3a70b74a322ede0490a15e7
Dmitriy Iskra <dmitriyiskra@mail.ru>
2024-12-19 14:45:35 +0300

    creating reviews modal

---
+++ src/js/product-card-reviews/ControllReviews.js
@@ -0,0 +1,22 @@
+export default class ControllReviews {
+    constructor(redraw) {
+        this.redraw = redraw;
+
+        this.click = this.click.bind(this);
+    }
+
+    init() {
+        this.registerEvents();
+    }
+
+    registerEvents() {
+        this.redraw.el.addEventListener('click', this.click);
+    }
+
+    click(e) {
+        e.preventDefault();
+        if(e.target.closest('')) {
+
+        }
+    }
+}
